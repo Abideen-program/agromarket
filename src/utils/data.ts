d1dfdfe852f827b4fbf935906e9d706638482b78
Abideen Olafimihan <olafimihana@gmail.com>
2024-06-20 15:20:22 +0100

    close icon added

---
+++ src/utils/data.ts
@@ -0,0 +1,8 @@
+export const navLinks = [
+  //   { id: 1, title: 'Home', path: '/'},
+  { id: 2, title: "About Us", path: "/about" },
+  { id: 3, title: "Marketplace", path: "/marketplace" },
+  { id: 4, title: "Update", path: "/update" },
+  { id: 6, title: "Community", path: "/community" },
+  { id: 7, title: "Contact Us", path: "/contactus" },
+];
--- src/utils/images.ts
+++ src/utils/images.ts
@@ -2,8 +2,10 @@ import Logo from "../assets/icons/logo.svg";
 import Hambuger from "../assets/icons/hambuger.svg";
 import ArrowRight from "../assets/icons/arrowright.svg";
 import Hero from "../assets/icons/hero.svg";
+import Close from "../assets/icons/close.svg";
 
 export const iconImages = {
+  Close,
   Hero,
   ArrowRight,
   Logo,
