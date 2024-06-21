34f5f42638bb6b128b8fc1dcc25c438e8088500b
Abideen Olafimihan <olafimihana@gmail.com>
2024-06-21 22:17:00 +0100

    data edited

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -1,8 +1,7 @@
 export const navLinks = [
-  //   { id: 1, title: 'Home', path: '/'},
-  { id: 2, title: "About Us", path: "/about" },
-  { id: 3, title: "Marketplace", path: "/marketplace" },
-  { id: 4, title: "Update", path: "/update" },
-  { id: 6, title: "Community", path: "/community" },
-  { id: 7, title: "Contact Us", path: "/contactus" },
+  { id: 1, title: "About Us", path: "/about" },
+  { id: 2, title: "Marketplace", path: "/marketplace" },
+  { id: 3, title: "Update", path: "/update" },
+  { id: 4, title: "Community", path: "/community" },
+  { id: 5, title: "Contact Us", path: "/contactus" },
 ];
